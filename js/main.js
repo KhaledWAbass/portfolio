ec22218ea371ec45224cb94f43b33d660bbcbe95
KhaledAbaas <khaledwalidabass@gmail.com>
2026-04-26 15:44:33 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const DOM = {
   Header: document.querySelector("header"),
   butnMenu: document.querySelector(".menu-btn"),
   left: document.querySelector(".pageShell .main .hero .left"),
+  up: document.querySelector(".pageShell .UP"),
   right: document.querySelector(".pageShell .main .hero .right"),
   progress: document.querySelectorAll(".skills .skill .prograss span"),
   skills: document.querySelector(
@@ -12,13 +13,34 @@ const DOM = {
   ),
   img: document.querySelector(".pageShell .main #about .about-content .img"),
   button: document.querySelector(".pageShell .main #projects button"),
+  git: document.querySelector(".pageShell .main #projects button.git"),
+  all: document.querySelector(".pageShell .main .all"),
   conProject: document.querySelector(".pageShell .main #projects .conProject"),
 };
 // event
-DOM.button.addEventListener(
-  "click",
-  () => (window.location = "/projects.html"),
-);
+DOM.button
+  ? DOM.button.addEventListener("click", () => window.open("projects.html"))
+  : null;
+// ==========================================
+DOM.git
+  ? DOM.button.addEventListener("click", () =>
+      window.open("https://github.com/KhaledWAbass"),
+    )
+  : null;
+// ==========================================
+DOM.up.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+if (DOM.all) {
+  document
+    .querySelectorAll(".pageShell .main .all .conProject .card")
+    .forEach((l) => {
+      l.classList.add("animation");
+    });
+}
 // code
 DOM.left ? DOM.left.classList.add("animation") : null;
 document;
@@ -35,14 +57,27 @@ DOM.butnMenu.addEventListener("click", () => {
   DOM.nav.classList.toggle("open");
 });
 window.addEventListener("scroll", () => {
-  if (scrollY > DOM.img.offsetTop - 400) {
-    DOM.img.classList.add("animation");
-  } else if (scrollY < DOM.img.offsetTop) {
-    // reorder
-    DOM.li.forEach((l) => {
-      l.classList.remove("active");
-    });
-    DOM.li[0].classList.add("active");
+  if (scrollY > 500) {
+    DOM.up.style.opacity = "1";
+    DOM.up.style.display = "flex";
+  } else {
+    DOM.up.style.opacity = "0";
+    DOM.up.style.display = "none";
+  }
+  if (DOM.img) {
+    if (scrollY > DOM.img.offsetTop - 400) {
+      DOM.img.classList.add("animation");
+    } else if (scrollY < DOM.img.offsetTop) {
+      // reorder
+      DOM.li.forEach((l) => {
+        l.classList.remove("active");
+      });
+      DOM.li[0].classList.add("active");
+    } else {
+      return;
+    }
+  } else {
+    return;
   }
   if (scrollY > DOM.skills.offsetTop - 600) {
     // reorder
@@ -60,6 +95,7 @@ window.addEventListener("scroll", () => {
       r.classList.add("fillbar");
     });
   }
+  console.log(DOM.conProject);
   if (scrollY > DOM.conProject.offsetTop - 400) {
     document
       .querySelectorAll(".pageShell .main #projects .conProject .card")
@@ -85,3 +121,13 @@ window.addEventListener("scroll", () => {
       });
   }
 });
+
+// links
+// https://khaledwabass.github.io/telepronter/
+// https://khaledwabass.github.io/Mystore/
+// https://khaledwabass.github.io/web/
+// https://khaledwabass.github.io/-/
+// https://khaledwabass.github.io/templet-1/
+// https://khaledwabass.github.io/templet-2/
+// https://khaledwabass.github.io/templet-3/
+// https://khaledwabass.github.io/khaled-dev.github.io/
